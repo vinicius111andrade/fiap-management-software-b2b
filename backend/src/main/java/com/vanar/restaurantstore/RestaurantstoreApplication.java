4df81647dabca2e178761b5a914dbd7052b907eb
vinicius111andrade <vinicius111andrade@gmail.com>
2024-05-19 17:32:31 -0300

    Adding spring boot project

---
+++ backend/src/main/java/com/vanar/restaurantstore/RestaurantstoreApplication.java
@@ -0,0 +1,13 @@
+package com.vanar.restaurantstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantstoreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vanar/restaurantstore/RestaurantstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanar.restaurantstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
